c3e7b5c41712c3ec953cb2773ef3c24a53655479
Yureshwar Ravuri <yureshwar@gmail.com>
2021-10-07 20:21:32 +0530

    Added the error logging functionality as per #47

--- poc-logging/js/injectlogtest.js
+++
@@ -1,6 +0,0 @@
-var s = document.createElement('script');
-var scriptpath = chrome.extension.getURL("js/logtest.js");
-s.src = scriptpath;
-s.onload = function() {
-};
-(document.body || document.documentElement).appendChild(s);
