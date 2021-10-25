8b3019707cc6e0e427c8fea25affce30f846f59e
Yureshwar Ravuri <yureshwar@gmail.com>
2021-10-25 17:56:59 +0530

    Added the fix for log4js as per #258

--- chrome-plugin/js/injectscriptmodifier.js
+++ chrome-plugin/js/injectscriptmodifier.js
@@ -1,11 +1,3 @@
-var loggerscript = document.createElement('script');
-var loggerscriptpath = chrome.extension.getURL('js/log4js/js/log4js.min.js');
-loggerscript.src = loggerscriptpath;
-loggerscript.async = false;
-loggerscript.onload = function() {
-};
-(document.head || document.documentElement).appendChild(loggerscript);
-
 var s = document.createElement('script');
 var scriptpath = chrome.extension.getURL('js/links.js');
 s.src = scriptpath;
