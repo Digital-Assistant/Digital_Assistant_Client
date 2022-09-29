933c433aeab67647fedcb27c82a7a06c4b7084b6
Lakshman Veti <lakshman.veti@gmail.com>
2022-09-29 10:33:57 +0530

    Background, Manifest file copy to build target fix

--- udan-react-ts/scripts/postbuild.js
+++ udan-react-ts/scripts/postbuild.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
+const targetDir = "./build", sourceDir = "./public";
 
-fs.readFile(`./build/assets/bundle.js`, 'utf-8', function (err, contents) {
+//replace few js error prone expressions of 3rd party package code
+fs.readFile(`${targetDir}/assets/bundle.js`, 'utf-8', function (err, contents) {
   if (err) {
     console.log(err);
     return;
@@ -9,9 +11,12 @@ fs.readFile(`./build/assets/bundle.js`, 'utf-8', function (err, contents) {
   replaced = replaced.replace(/win\.location\.href/gi, 'win?.location?.href');
   replaced = replaced.replace(/setImmediate/gi, 'setTimeout');
   
-  fs.writeFile(`./build/assets/bundle.js`, replaced, 'utf-8', function (e) {
+  fs.writeFile(`${targetDir}/assets/bundle.js`, replaced, 'utf-8', function (e) {
     //console.log(e);
   });
 });
 
 
+//copy background, manifest files to build from public folder
+fs.writeFileSync(`${targetDir}/background.js`, fs.readFileSync(`${sourceDir}/background.js`));
+fs.writeFileSync(`${targetDir}/manifest.json`, fs.readFileSync(`${sourceDir}/manifest.json`));
