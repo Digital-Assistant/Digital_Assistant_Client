00f9378141e4b16296ddc271524734ee8bc9d2f9
Yureswar Ravuri <yureshwar@gmail.com>
2023-01-06 18:07:13 +0530

    Merged SDK to react typescript branch

---
+++ dist/InjectHeaders.js
@@ -0,0 +1,1 @@
+let s=document.createElement("script"),scriptpath=chrome?.runtime?.getURL("assets/UDAHeaders.js");s.src=scriptpath,s.onload=function(){},(document.head||document.documentElement).appendChild(s);
