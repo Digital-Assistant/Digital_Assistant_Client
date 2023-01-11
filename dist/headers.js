52b87936187862e426d8cce2322105776fb6c455
Mohd Rejoan <wearecrx@gmail.com>
2023-01-11 12:45:14 +0600

    change extension mount point from body to custom tag with shadow-dom

--- dist/headers.js
+++ dist/headers.js
@@ -1,1 +1,5 @@
-let s=document.createElement("script"),scriptpath=chrome?.runtime?.getURL("assets/UdanHeaders.js");s.src=scriptpath,s.onload=function(){},(document.head||document.documentElement).appendChild(s);
+let s = document.createElement("script"),
+    scriptpath = chrome?.runtime?.getURL("assets/UdanHeaders.js");
+(s.src = scriptpath),
+    (s.onload = function () {}),
+    (document.head || document.documentElement).appendChild(s);
--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,71 @@ import { createRoot } from "react-dom/client";
 
 import "./util/i18n";
 import App from "./App";
+import ReactDOM from "react-dom";
 
-const body = document.querySelector("body");
+// const body = document.querySelector("body");
 
-const app = document.createElement("div");
+// const app = document.createElement("div");
 
-app.id = "udan-react-root";
-app.classList.add('uda_exclude');
+// app.id = "udan-react-root";
+// app.classList.add('uda_exclude');
 
-if (body) {
-  body.appendChild(app);
+// if (body) {
+//   body.appendChild(app);
+// }
+
+// const container = document.getElementById("udan-react-root");
+// const root = createRoot(container!);
+
+// root.render(<App />);
+
+// define custom tag constructor
+class CustomTag extends HTMLElement {
+    constructor() {
+        super();
+        // attach shadow
+        this.attachShadow({ mode: "open" });
+        const div = document.createElement("div");
+        div.id = "udan-react-root";
+        div.classList.add("uda_exclude");
+        this.shadowRoot.appendChild(div);
+        // ReactDOM.render(<App />, div)
+        const reactRoot = createRoot(div);
+        reactRoot.render(<App />)
+    }
+
+    // getRoot() {
+    //     return this.shadowRoot.querySelector("#udan-react-root");
+    // }
 }
 
-const container = document.getElementById("udan-react-root");
-const root = createRoot(container!);
+// define custom tag
+customElements.define("udan-shadow", CustomTag);
 
-root.render(<App />);
+// get html
+const htmlTag = document.querySelector("html");
+
+// create udan custom container and push udan shadow
+const udanContainer = document.createElement("udan-container");
+const udanShadow = document.createElement("udan-shadow");
+
+// push custom tag serially udan-shadow > udan-container > html-tag
+udanContainer.appendChild(udanShadow);
+
+// check if html available
+if (htmlTag) {
+    htmlTag.appendChild(udanContainer);
+}
+
+// render in react
+
+// const rootObj = new CustomTag();
+// const rootTag = rootObj.getRoot();
+
+// console.log(document.querySelector('div'));
+// console.log('custom tag', rootTag);
+
+// const reactRoot = createRoot(rootTag);
+// reactRoot.render(<App />)
+
+// root.render(<App />);
