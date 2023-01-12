89ff040fbc2e4485f14b9cca60e7602d43ae0e21
Mohd Rejoan <wearecrx@gmail.com>
2023-01-13 01:15:11 +0600

    generate css external file from webpack

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,7 @@ import { createRoot } from "react-dom/client";
 
 import "./util/i18n";
 import App from "./App";
-import ReactDOM from "react-dom";
 
-// const body = document.querySelector("body");
-
-// const app = document.createElement("div");
-
-// app.id = "udan-react-root";
-// app.classList.add('uda_exclude');
-
-// if (body) {
-//   body.appendChild(app);
-// }
-
-// const container = document.getElementById("udan-react-root");
-// const root = createRoot(container!);
-
-// root.render(<App />);
 
 // define custom tag constructor
 class CustomTag extends HTMLElement {
@@ -34,14 +18,9 @@ class CustomTag extends HTMLElement {
         div.id = "udan-react-root";
         div.classList.add("uda_exclude");
         this.shadowRoot.appendChild(div);
-        // ReactDOM.render(<App />, div)
         const reactRoot = createRoot(div);
         reactRoot.render(<App />)
     }
-
-    // getRoot() {
-    //     return this.shadowRoot.querySelector("#udan-react-root");
-    // }
 }
 
 // define custom tag
@@ -62,15 +41,3 @@ if (htmlTag) {
     htmlTag.appendChild(udanContainer);
 }
 
-// render in react
-
-// const rootObj = new CustomTag();
-// const rootTag = rootObj.getRoot();
-
-// console.log(document.querySelector('div'));
-// console.log('custom tag', rootTag);
-
-// const reactRoot = createRoot(rootTag);
-// reactRoot.render(<App />)
-
-// root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,8 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = (env, argv) => {
 
@@ -28,23 +30,22 @@ module.exports = (env, argv) => {
     const buildPath = (env.build && env.build === 'production') ? 'dist' : 'build';
 
     const webpackConfig = {
-
         entry: {
             // string | object | array
             // defaults to ./src
             // Here the application starts executing
             // and webpack starts bundling
-            'UDAHeaders': './src/Headers.js',
-            'UDASdk': './src/index.tsx',
-            'UDABackground': './src/Background.js',
-            'UDALoad': './src/InjectSDK.js'
+            UDAHeaders: "./src/Headers.js",
+            UDASdk: "./src/index.tsx",
+            UDABackground: "./src/Background.js",
+            UDALoad: "./src/InjectSDK.js",
         },
-        mode: 'development',// "production" | "development" | "none"
-        devtool: 'cheap-module-source-map',// enum
+        mode: "development", // "production" | "development" | "none"
+        devtool: "cheap-module-source-map", // enum
         watch: false,
 
         watchOptions: {
-            ignored: '/node_modules/',
+            ignored: "/node_modules/",
         },
 
         module: {
@@ -55,23 +56,23 @@ module.exports = (env, argv) => {
                     // Conditions:
                     test: /\.(js|jsx)$/,
                     exclude: /(node_modules|bower_components)/,
-                    loader: 'babel-loader', // the loader which should be applied, it'll be resolved relative to the context
-                    options: {presets: ['@babel/env', '@babel/preset-react']},  // options for the loader
+                    loader: "babel-loader", // the loader which should be applied, it'll be resolved relative to the context
+                    options: { presets: ["@babel/env", "@babel/preset-react"] }, // options for the loader
                 },
                 {
                     test: /\.(ts|tsx)$/,
                     exclude: /node_modules/,
-                    loader: 'ts-loader',
+                    loader: "ts-loader",
                     options: {
-                        getCustomTransformers: program => ({
-                            before: [transform(program)]
-                        })
-                    }
+                        getCustomTransformers: (program) => ({
+                            before: [transform(program)],
+                        }),
+                    },
                 },
                 {
                     // Conditions:
                     test: /\.css$/,
-                    use: ['style-loader', 'css-loader']  // When multiple loader configuration needed
+                    use: ["style-loader", "css-loader"], // When multiple loader configuration needed
                 },
                 {
                     // Conditions:
@@ -83,10 +84,22 @@ module.exports = (env, argv) => {
                              loader: 'file-loader',
                              options: {name: '[name].min.css'}
                          },*/
-                        'style-loader',
-                        'css-loader',
-                        'sass-loader'
-                    ]
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
                 },
                 {
                     test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
@@ -96,40 +109,42 @@ module.exports = (env, argv) => {
                 {
                     // Conditions:
                     test: /\.(jpe?g|png|gif|svg)$/i,
-                    use: [{
-                        // When multiple loader configuration needed
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: '/images/'
-                        }
-                    }]
+                    use: [
+                        {
+                            // When multiple loader configuration needed
+                            loader: "file-loader",
+                            options: {
+                                name: "[name].[ext]",
+                                outputPath: "/images/",
+                            },
+                        },
+                    ],
                 },
                 {
                     test: /\.svg$/,
                     exclude: /node_modules/,
                     use: {
                         // When multiple loader configuration needed
-                        loader: 'svg-url-loader',
-                        options: {name: '[name].svg'}
-                    }
-                }
+                        loader: "svg-url-loader",
+                        options: { name: "[name].svg" },
+                    },
+                },
             ],
         },
         plugins: [
             // list of additional plugins
             new webpack.ProvidePlugin({
-                process: 'process/browser',
-                Buffer: ['buffer', 'Buffer']
+                process: "process/browser",
+                Buffer: ["buffer", "Buffer"],
             }),
             new CopyPlugin({
                 patterns: [
-                    {from: 'src/logo.*', to: "../logos/[name][ext]"},
-                    {from: 'public/', to: "../"}
+                    { from: "src/logo.*", to: "../logos/[name][ext]" },
+                    { from: "public/", to: "../" },
                 ],
             }),
             new webpack.DefinePlugin({
-                "process.env": JSON.stringify(process.env)
+                "process.env": JSON.stringify(process.env),
             }),
             new Dotenv({
                 path: `${envFile}`,
@@ -139,6 +154,9 @@ module.exports = (env, argv) => {
                 silent: true, // hide any errors
                 defaults: false,
                 ignoreStub: true,
+            }),
+            new MiniCssExtractPlugin({
+                filename: "styles.css",
             }),
             // new BundleAnalyzerPlugin(),
             // gzip
@@ -151,25 +169,26 @@ module.exports = (env, argv) => {
         resolve: {
             // options for resolving module requests
             // (does not apply to resolving of loaders)
-            extensions: ['.tsx', '.ts', '.js', '.css', '.scss'], // extensions that are used
-            modules: ['./node_modules'], // directories where to look for modules (in order)
+            extensions: [".tsx", ".ts", ".js", ".css", ".scss"], // extensions that are used
+            modules: ["./node_modules"], // directories where to look for modules (in order)
             alias: {
                 // a list of module name aliases
                 // aliases are imported relative to the current context
                 process: "process/browser",
-                utils: path.resolve(__dirname, './src/config/index')
+                utils: path.resolve(__dirname, "./src/config/index"),
             },
-            fallback: { //fallback module dependencies
-                "fs": false,
-                "http": require.resolve("stream-http"),
-                "https": require.resolve("https-browserify"),
-                "stream": require.resolve("stream-browserify"),
-                "zlib": require.resolve("browserify-zlib"),
-                "buffer": require.resolve("buffer/"),
-                "path": require.resolve("path-browserify"),
-                "os": require.resolve("os-browserify/browser"),
-                "assert": require.resolve("assert/")
-            }
+            fallback: {
+                //fallback module dependencies
+                fs: false,
+                http: require.resolve("stream-http"),
+                https: require.resolve("https-browserify"),
+                stream: require.resolve("stream-browserify"),
+                zlib: require.resolve("browserify-zlib"),
+                buffer: require.resolve("buffer/"),
+                path: require.resolve("path-browserify"),
+                os: require.resolve("os-browserify/browser"),
+                assert: require.resolve("assert/"),
+            },
         },
         optimization: {
             minimizer: [
@@ -185,13 +204,13 @@ module.exports = (env, argv) => {
         },
         output: {
             // options related to how webpack emits results
-            publicPath: '',
-            filename: '[name].js', // the filename template for entry chunks
-            library: 'UdanLibrary',
-            libraryTarget: 'var',
-            path: path.resolve(__dirname, buildPath+'/assets'),  // the target directory for all output files
+            publicPath: "",
+            filename: "[name].js", // the filename template for entry chunks
+            library: "UdanLibrary",
+            libraryTarget: "var",
+            path: path.resolve(__dirname, buildPath + "/assets"), // the target directory for all output files
             // must be an absolute path (use the Node.js path module)
-            clean: true
+            clean: true,
         },
     };
 
