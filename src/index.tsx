af8312d75fe70547229beb7f650aca50029e976a
Mohd Rejoan <wearecrx@gmail.com>
2023-01-16 16:16:31 +0600

    shadow constructor remove and implemented on root div

--- src/index.tsx
+++ src/index.tsx
@@ -7,37 +7,32 @@ import { createRoot } from "react-dom/client";
 import "./util/i18n";
 import App from "./App";
 
-
-// define custom tag constructor
-class CustomTag extends HTMLElement {
-    constructor() {
-        super();
-        // attach shadow
-        this.attachShadow({ mode: "open" });
-        const div = document.createElement("div");
-        div.id = "udan-react-root";
-        div.classList.add("uda_exclude");
-        this.shadowRoot.appendChild(div);
-        const reactRoot = createRoot(div);
-        reactRoot.render(<App />)
-    }
-}
-
-// define custom tag
-customElements.define("udan-shadow", CustomTag);
-
 // get html
 const htmlTag = document.querySelector("html");
 
 // create udan custom container and push udan shadow
-const udanContainer = document.createElement("udan-container");
-const udanShadow = document.createElement("udan-shadow");
+const udanContainer = document.createElement("udan");
+// react mounter
+const rootDiv = document.createElement("div");
+rootDiv.id = "udan-react-root";
+rootDiv.classList.add("uda_exclude");
+const rootShadow = rootDiv.attachShadow({ mode: "open" });
+udanContainer.appendChild(rootDiv);
 
-// push custom tag serially udan-shadow > udan-container > html-tag
-udanContainer.appendChild(udanShadow);
+// render react
+const reactRoot = createRoot(rootShadow);
+reactRoot.render(<App />);
 
 // check if html available
 if (htmlTag) {
     htmlTag.appendChild(udanContainer);
 }
 
+// const div = document.createElement("div");
+// div.id = "udan-react-root";
+// div.classList.add("uda_exclude");
+// const reactRoot = createRoot(div);
+// reactRoot.render(<App />);
+
+// push custom tag serially udan-shadow > udan-container > html-tag
+// udanContainer.appendChild(udanShadow);
