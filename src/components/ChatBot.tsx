e834dfa7b9c49e0f40eb9f8308dbf9f3337705ab
Yureswar Ravuri <yureshwar@gmail.com>
2023-04-28 10:51:16 +0530

    Integrated chatbot to UDA

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,13 +1,8 @@
-import '../chatbot/runtime.js';
-import '../chatbot/polyfills.js';
-import '../chatbot/main.js';
-import '../chatbot/styles.css';
 import React, {useEffect} from "react";
 
 import {ParentCompProps} from "../interfaces/ParentCompProps";
 
 export const ChatBot: React.FC<ParentCompProps> = (props) => {
-  const {content} = props;
 
   let shadowRoot: any;
 
@@ -15,17 +10,39 @@ export const ChatBot: React.FC<ParentCompProps> = (props) => {
     // get shadow root element
     shadowRoot = document.getElementById('udan-react-root').shadowRoot;
 
+    let chatBotDiv = shadowRoot.getElementById('chat-bot-root');
+
     // Create div element for react to render into
-    const angularDiv = document.createElement('chat-bot-root');
-    angularDiv.setAttribute('id', 'chat-bot-root');
+    // const angularDiv = document.createElement('chat-bot-root');
+    // angularDiv.setAttribute('id', 'chat-bot-root');
 
     // Append react root to shadow root
-    shadowRoot.appendChild(angularDiv);
+    // shadowRoot.appendChild(angularDiv);
+
+    /*let chatBotScripts = [
+      'runtime.js',
+      'polyfills.js',
+      'main.js'
+    ]
+
+    for(let chatBotScript of chatBotScripts){
+      const scriptDiv: any = document.createElement('script');
+      scriptDiv.src = "../chatbot/"+chatBotScript;
+      chatBotDiv.appendChild(scriptDiv);
+    }
+    */
+
+    require('../chatbot/runtime.js');
+    require('../chatbot/polyfills.js');
+    require('../chatbot/main.js');
+
   }
 
   useEffect(()=>{
     createRootElement();
   },[])
 
+  // return <div id="chat-bot-root"></div>;
+  // return React.createElement('chat-bot-root', {id:"chat-bot-root"});
   return <></>;
 }
--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 import {ParentCompProps} from "../../interfaces/ParentCompProps";
+import {Tabs, TabsProps} from 'antd';
 import {ChatBot} from "../ChatBot";
 
 const Body: React.FC<ParentCompProps> = (props) => {
   const { content } = props;
+
+  const chatBotDiv = () => {
+    return React.createElement('chat-bot-root', {id:"chat-bot-root", class:"chat-bot-root"}, <ChatBot></ChatBot>);
+  }
+
+  const items: TabsProps['items'] = [
+    {
+      key: '1',
+      label: `UDAN`,
+      children: content,
+    },
+    {
+      key: '2',
+      label: `SMARTBOT`,
+      children: chatBotDiv() ,
+    }
+  ];
+
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+
   return (
     <div
       className="uda-container uda-clear uda-cards-scroller uda_exclude"
       id="uda-content-container"
     >
-      {content}
-      <ChatBot></ChatBot>
+      {/*{content}*/}
+      {/*<ChatBot></ChatBot>*/}
+      <Tabs defaultActiveKey="1" centered size={`large`} items={items} onChange={onChange} />
     </div>
   );
 };
 
+// @ts-ignore
 export default Body;
