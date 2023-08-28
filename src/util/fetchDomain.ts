9960ccf04a15304403948e90d0f1bf74d5442eae
Yureshwar Ravuri <yureshwar@gmail.com>
2023-08-28 22:10:53 +0530

    Added changes as per issue #449

---
+++ src/util/fetchDomain.ts
@@ -0,0 +1,24 @@
+import { parseDomain, ParseResultType } from "parse-domain";
+export const fetchDomain = () => {
+    let finalDomain = window.location.host;
+    if(global.UDAGlobalConfig.enableForAllDomains){
+        const parseResult = parseDomain(finalDomain);
+        switch (parseResult.type) {
+            case ParseResultType.Listed: {
+                const { hostname, domain, subDomains, topLevelDomains } = parseResult;
+                finalDomain = domain+'.'+topLevelDomains.join('.');
+                break;
+            }
+            case ParseResultType.Reserved:
+            case ParseResultType.NotListed: {
+                const { hostname } = parseResult;
+                console.log(`${hostname} is a reserved or unknown domain`);
+                break;
+            }
+            default:
+                const { hostname } = parseResult;
+                console.log(hostname);
+        }
+    }
+    return finalDomain;
+}
