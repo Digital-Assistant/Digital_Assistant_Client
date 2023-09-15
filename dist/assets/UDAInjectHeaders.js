1e87c6e94a814a40c82fc64bb4e3f28418c2bad8
Yureshwar Ravuri <yureshwar@gmail.com>
2023-09-15 16:51:48 +0530

    Added new build for issue-452

---
+++ dist/assets/UDAInjectHeaders.js
@@ -0,0 +1,16 @@
+var UdanLibrary;
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/InjectHeaders.js ***!
+  \******************************/
+var _chrome, _chrome$runtime;
+var s = document.createElement('script');
+var scriptpath = (_chrome = chrome) === null || _chrome === void 0 ? void 0 : (_chrome$runtime = _chrome.runtime) === null || _chrome$runtime === void 0 ? void 0 : _chrome$runtime.getURL("assets/UDAHeaders.js");
+s.src = scriptpath;
+s.onload = function () {};
+(document.head || document.documentElement).appendChild(s);
+UdanLibrary = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=UDAInjectHeaders.js.map
