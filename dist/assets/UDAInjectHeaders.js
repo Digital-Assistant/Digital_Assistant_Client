ae8ec6ea584907dd0bcb6eeae4827ea6e1a57686
Yureshwar Ravuri <yureshwar@gmail.com>
2023-09-19 11:56:59 +0530

    Merged issue #452, #442 and added new build for the master branch

--- dist/assets/UDAInjectHeaders.js
+++ dist/assets/UDAInjectHeaders.js
@@ -1,16 +1,1 @@
-var UdanLibrary;
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************!*\
-  !*** ./src/InjectHeaders.js ***!
-  \******************************/
-var _chrome, _chrome$runtime;
-var s = document.createElement('script');
-var scriptpath = (_chrome = chrome) === null || _chrome === void 0 ? void 0 : (_chrome$runtime = _chrome.runtime) === null || _chrome$runtime === void 0 ? void 0 : _chrome$runtime.getURL("assets/UDAHeaders.js");
-s.src = scriptpath;
-s.onload = function () {};
-(document.head || document.documentElement).appendChild(s);
-UdanLibrary = __webpack_exports__;
-/******/ })()
-;
-//# sourceMappingURL=UDAInjectHeaders.js.map
+var UdanLibrary;!function(){var e,n,d=document.createElement("script"),t=null===(e=chrome)||void 0===e||null===(n=e.runtime)||void 0===n?void 0:n.getURL("assets/UDAHeaders.js");d.src=t,d.onload=function(){},(document.head||document.documentElement).appendChild(d),UdanLibrary={}}();
