2bbe2afd8339c2102c0f8de9bc88a3fe94fec1ee
Yureshwar Ravuri <yureshwar@gmail.com>
2023-12-14 11:06:26 +0530

    Added changes for the bug fix and added test cases.

--- src/util/initSpecialNodes.ts
+++ src/util/initSpecialNodes.ts
@@ -11,27 +11,34 @@ declare global {
 }
 
 /**
- * Set ignorable classes for Udan con
+ * Initialize special nodes for Udan con.
+ * Fetches special nodes for the REST service and sets ignorable classes.
  */
 export const initSpecialNodes = async () => {
-
-  //fetch special nodes for REST service
+  // Fetch special nodes for REST service
   global.udaSpecialNodes = await fetchSpecialNodes();
 
+  // Get all children of Uda container
   const children = getAllChildren(
       document.querySelector(`.${CONFIG.UDA_CONTAINER_CLASS}`)
   );
+
+  // Iterate through each child
   for (let i = 0; i < children?.length; i++) {
     try {
+      // Check if the child is not excluded and not already ignored
       if (
           children[i] &&
           !global.udaSpecialNodes?.exclude?.tags?.includes(
               children[i]?.tagName?.trim()?.toLocaleLowerCase()
           ) &&
           children[i].className.indexOf(CONFIG.UDA_CLICK_IGNORE_CLASS) == -1
-      )
+      ) {
+        // Add UDA click ignore class to the child
         children[i].className += " " + CONFIG.UDA_CLICK_IGNORE_CLASS;
+      }
     } catch (e) {
+      // Handle any errors
     }
   }
 };
