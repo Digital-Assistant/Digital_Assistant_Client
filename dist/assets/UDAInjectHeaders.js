b7dc0d8fe36f6630e059e96524e084b180d45497
Yureshwar Ravuri <yureshwar@gmail.com>
2024-02-04 18:12:46 +0530

    Added new build for master branch.

--- dist/assets/UDAInjectHeaders.js
+++ dist/assets/UDAInjectHeaders.js
@@ -1,1 +1,1 @@
-var UdanLibrary;!function(){var e,n=document.createElement("script"),d=null===(e=chrome)||void 0===e||null===(e=e.runtime)||void 0===e?void 0:e.getURL("assets/UDAHeaders.js");n.src=d,n.onload=function(){},(document.head||document.documentElement).appendChild(n),UdanLibrary={}}();
+var UdanLibrary;(()=>{var e;let n=document.createElement("script"),d=null===(e=chrome)||void 0===e||null===(e=e.runtime)||void 0===e?void 0:e.getURL("assets/UDAHeaders.js");n.src=d,n.onload=function(){},(document.head||document.documentElement).appendChild(n),UdanLibrary={}})();
