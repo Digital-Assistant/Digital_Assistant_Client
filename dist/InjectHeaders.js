a3dd81135e08824e0d965d609b112a2d0fa56c26
KC <kc@nistapp.com>
2024-08-14 23:26:21 +0530

    revamped webpack. Modular, maintainable, multi-threaded, caching etc. Also added dist to .gitignore. Could not get bundleanalyser working. Also need to implement npm run clean and npm run cache-clear. Had issues with rimraf and del modules

--- dist/InjectHeaders.js
+++
@@ -1,1 +0,0 @@
-let s=document.createElement("script"),scriptpath=chrome?.runtime?.getURL("assets/UDAHeaders.js");s.src=scriptpath,s.onload=function(){},(document.head||document.documentElement).appendChild(s);
