f8b28b224dc9e5ef955a51ff88f5aceae5c3e932
Yureshwar Ravuri <yureshwar@gmail.com>
2024-09-23 11:39:52 +0530

    Added new build for qa

--- dist/InjectHeaders.js
+++
@@ -1,6 +0,0 @@
-let s = document.createElement('script');
-let scriptpath = chrome?.runtime?.getURL("assets/UDAHeaders.js");
-s.src = scriptpath;
-s.onload = function() {
-};
-(document.head || document.documentElement).appendChild(s);
--- dist/src/config/keycloak.js
+++
@@ -1,8 +0,0 @@
-import Keycloak from "keycloak-js";
-const keycloak = new Keycloak({
-    url: process.env.keyCloakUrl,
-    realm: "UDAN",
-    clientId: "react-app"
-});
-export default keycloak;
-//# sourceMappingURL=keycloak.js.map
