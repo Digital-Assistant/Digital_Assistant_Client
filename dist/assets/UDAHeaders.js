6cdf53dfb8e609f39a378718e3a37a80c81d4448
Yureshwar Ravuri <yureshwar@gmail.com>
2024-11-25 19:39:58 +0530

    Added build for screen resolution changes.

--- dist/assets/UDAHeaders.js
+++ dist/assets/UDAHeaders.js
@@ -1,135 +1,27 @@
 var UdanLibrary;
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/util/headers/addToClickObject.js":
-/*!**********************************************!*\
-  !*** ./src/util/headers/addToClickObject.js ***!
-  \**********************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ "./src/Headers.js":
+/*!************************!*\
+  !*** ./src/Headers.js ***!
+  \************************/
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   AddToClickObjects: () => (/* binding */ AddToClickObjects),
-/* harmony export */   ignoreTags: () => (/* binding */ ignoreTags)
-/* harmony export */ });
-const ignoreTags = ["body", "document", "window", "html", "script", "style", "iframe", "doctype", "link", "svg", "path", "meta", "circle", "rect", "stop", "defs", "linearGradient", "g"];
-const AddToClickObjects = node => {
-  try {
-    let clickObject = {
-      element: node,
-      id: ''
-    };
-
-    //checking whether the element is window or not
-    if (clickObject.element === window) {
-      return;
-    }
-    if (typeof clickObject?.element?.tagName === 'undefined') {
-      return;
-    }
-    let tag = clickObject?.element?.tagName;
-    if (tag && ignoreTags.indexOf(tag.toLowerCase()) !== -1) {
-      return;
-    }
-    if (node.classList && node.classList.contains('uda_exclude')) {
-      return;
-    }
-    for (let i = 0; i < UDAClickObjects.length; i++) {
-      if (UDAClickObjects[i].element.isSameNode(clickObject.element)) {
-        //todo, discuss , how better to call actions, if multiple actions should be stored, or selector better.
-        return;
-      }
-    }
-    clickObject.id = UDAClickObjects.length;
-    UDAClickObjects.push(clickObject);
-  } catch (e) {
-    console.log('Unable to process clickable object - ' + node, e);
-  }
-};
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: .env[\"presets\"] must be an object, or undefined\n    at /home/yureshwar/Desktop/nistapp/projects/UDAN/UDAN-Frontend/babel.config.js\n    at Object.loadPartialConfigAsync (/home/yureshwar/Desktop/nistapp/projects/UDAN/UDAN-Frontend/node_modules/@babel/core/lib/config/index.js:34:85)\n    at Object.loader (/home/yureshwar/Desktop/nistapp/projects/UDAN/UDAN-Frontend/node_modules/babel-loader/lib/index.js:109:30)\n    at Object.<anonymous> (/home/yureshwar/Desktop/nistapp/projects/UDAN/UDAN-Frontend/node_modules/babel-loader/lib/index.js:39:12)");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
 /******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/Headers.js"]();
+/******/ 	UdanLibrary = __webpack_exports__;
 /******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-/*!************************!*\
-  !*** ./src/Headers.js ***!
-  \************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _util_headers_addToClickObject__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./util/headers/addToClickObject */ "./src/util/headers/addToClickObject.js");
-// adding the click object that is registered via javascript
-window.UDAClickObjects = [];
-window.UDARemovedClickObjects = [];
-
-EventTarget.prototype.addEventListener = function (addEventListener) {
-  return function () {
-    if (arguments[0] === "click") {
-      (0,_util_headers_addToClickObject__WEBPACK_IMPORTED_MODULE_0__.AddToClickObjects)(this);
-    }
-    addEventListener.call(this, arguments[0], arguments[1], arguments[2]);
-  };
-}(EventTarget.prototype.addEventListener);
-})();
-
-UdanLibrary = __webpack_exports__;
 /******/ })()
 ;
 //# sourceMappingURL=UDAHeaders.js.map
