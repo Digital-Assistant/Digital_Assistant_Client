d289aa1c6924405807da9eb3f27d002d1b69893b
Yureshwar Ravuri <yureshwar@gmail.com>
2025-05-28 20:10:55 +0530

    Added build for testing.

--- dist/InjectSDK.js
+++ dist/InjectSDK.js
@@ -1,1 +1,41 @@
-let s1=document.createElement("script"),scriptPath=chrome?.runtime?.getURL("assets/UDASdk.js");s1.src=scriptPath,s1.onload=function(){},(document.body||document.documentElement).appendChild(s1),document.addEventListener("RequestUDASessionData",(function(e){chrome.runtime.sendMessage({action:e.detail.data,data:e.detail.data})})),document.addEventListener("UDADebugSetEvent",(function(e){chrome.runtime.sendMessage({action:e.detail.data.action,data:e.detail.data.value})})),document.addEventListener("CreateUDASessionData",(function(e){chrome.runtime.sendMessage({action:e.detail.action,data:e.detail.data})})),document.addEventListener("UDANodeData",(function(e){document.dispatchEvent(new CustomEvent("UDASaveNodeData",{detail:{data:{jsonData:domJSON.toJSON(e.relatedTarget)}}}))})),chrome.runtime.onMessage.addListener((function(e){"UDAUserSessionData"===e.action?document.dispatchEvent(new CustomEvent("UDAUserSessionData",{detail:{data:e.data},bubbles:!1,cancelable:!1})):"UDAAuthenticatedUserSessionData"===e.action?document.dispatchEvent(new CustomEvent("UDAAuthenticatedUserSessionData",{detail:{data:e.data},bubbles:!1,cancelable:!1})):"UDAAlertMessageData"===e.action&&document.dispatchEvent(new CustomEvent("UDAAlertMessageData",{detail:{data:e.data},bubbles:!1,cancelable:!1}))}));
+let s1 = document.createElement('script');
+let scriptPath = chrome?.runtime?.getURL("assets/UDASdk.js");
+s1.src = scriptPath;
+s1.onload = function() {
+};
+(document.body || document.documentElement).appendChild(s1);
+
+document.addEventListener("RequestUDASessionData", function(data) {
+    chrome.runtime.sendMessage({action:data.detail.data,data:data.detail.data});
+});
+
+document.addEventListener("UDADebugSetEvent", function(data) {
+    chrome.runtime.sendMessage({action:data.detail.data.action,data:data.detail.data.value});
+});
+
+/**
+ * create keycloak storage to chrome
+ */
+document.addEventListener("CreateUDASessionData", function(data) {
+    chrome.runtime.sendMessage({action:data.detail.action,data:data.detail.data});
+});
+
+/**
+ * listener for removed additional data that was added by the frameworks
+ */
+document.addEventListener("UDANodeData", function(data) {
+    console.log('nodedata listener');
+    console.log({node: data.relatedTarget});
+    // document.dispatchEvent(new MouseEvent("UDASaveNodeData", {relatedTarget: data.relatedTarget}));
+    document.dispatchEvent(new CustomEvent("UDASaveNodeData",{detail: {data: {jsonData: domJSON.toJSON(data.relatedTarget)}}}));
+});
+
+chrome.runtime.onMessage.addListener(function(request) {
+    if(request.action==="UDAUserSessionData"){
+        document.dispatchEvent(new CustomEvent("UDAUserSessionData", {detail: {data: request.data}, bubbles: false, cancelable: false}));
+    } else if(request.action==="UDAAuthenticatedUserSessionData"){
+        document.dispatchEvent(new CustomEvent("UDAAuthenticatedUserSessionData", {detail: {data: request.data}, bubbles: false, cancelable: false}));
+    } else if(request.action==="UDAAlertMessageData"){
+        document.dispatchEvent(new CustomEvent("UDAAlertMessageData", {detail: {data: request.data}, bubbles: false, cancelable: false}));
+    }
+});
